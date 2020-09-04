dddab8c9b1ac5e4ecfb929d35dae863548e94928
Priya Lakshmipathi <Priya.Lakshmipathi@agilysys.com>
2020-09-04 19:12:50 +0530

    For dashboard changes

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -100,7 +100,7 @@ export class MenuComponent implements OnInit, AfterViewInit, OnDestroy {
     }
     this._featureFlagService.RefreshConfig();
     
-    if (!sessionStorage.getItem("EatecURI")) {
+    if (!sessionStorage.getItem("isEatecEnabled")) {
       var configValue = "";
       let featureNames = ["Enhanced Inventory"];
       this._propertyFeatureService.GetFeatureConfigurations(featureNames).then((featureconfigurations) => {
