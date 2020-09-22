d7b9098b880056d08f1ee7463102cb5975b925ec
Aravind Nagarajan <Aravind.Nagarajan@agilysys.com>
2020-09-22 20:44:41 +0530

    commit for auto log off issue fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { AppBusiness } from './app.business';
+import { ManageSessionService } from './login/manage-session.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,28 @@ import { AppBusiness } from './app.business';
 export class AppComponent {
   title = 'Retail-UI';
   constructor(
-    private appBusiness: AppBusiness
+    private appBusiness: AppBusiness,
+    private sessionService: ManageSessionService,
     ) {
 
+  }
+
+  /**
+   * @method @HostListener
+   * @description If listener watch the Entire Application mousemove and keydown Event
+   */
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e) {
+    if (this.sessionService.resetOnTrigger) {
+      this.sessionService.resetTimer();
+    }
+
+  }
+
+  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+    if (this.sessionService.resetOnTrigger) {
+      this.sessionService.resetTimer();
+    }
   }
 }
