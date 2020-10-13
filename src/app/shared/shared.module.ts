c124d891cd7a57dd3e85f3feb34b6c373b99750b
Vinayagam, Hariharan <Hariharan.Vinayagam@agilysys.com>
2020-10-13 04:08:35 -0400

    Merged PR 14725: 44151 Webapps Injector console error fixes
    Related work items: #44151

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -74,7 +74,7 @@ import { NotificationDataService } from './data-services/notification.data.servi
     RetailSharedModule,
     CommonSharedModule,
     GooglePlaceModule,
-    NgxMaterialTimepickerModule
+    NgxMaterialTimepickerModule.forRoot()
   ],
   providers: [
     TenantManagementCommunication,
