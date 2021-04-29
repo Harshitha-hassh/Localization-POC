edf2f77b1df5910086e4f6b02c72409023cbc9a7
Rajasekaran Ellappan <Rajasekaran.Ellappan@agilysys.com>
2021-04-29 15:09:17 +0530

    Retail card capture component integration changes

--- src/app/client/client-popup/create-client/additional-information/additional-information.component.ts
+++ src/app/client/client-popup/create-client/additional-information/additional-information.component.ts
@@ -36,11 +36,9 @@ export class AdditionalInformationComponent implements OnInit, OnDestroy {
   customFieldValues: any = [[], [], []];
   captions: any;
   showToggle: boolean;
-  cardInfo: ClientCreditCardInfo[] = [];
-  displayCardInfo: CardInfo;
+  cardInfo: ClientCreditCardInfo[] = [];  
   formSubscription: ISubscription;
-  selectedDevice: string;
-  availableDevices: Device[] = [];
+  
   cardType = 'visa';
   EncryptedCardData: string = '';
   userSessionConfiguration: UserSessionConfiguration;
@@ -49,6 +47,7 @@ export class AdditionalInformationComponent implements OnInit, OnDestroy {
   isFirstTime: boolean = true;
   isClientViewOnly = false;
   additionalInfo :any;
+  PaymentReferenceID = 0;
   @Input('inputData')
   set formData(value) {
     if(value && value.data!='')
@@ -89,193 +88,14 @@ export class AdditionalInformationComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnInit() {
-    this.initializeFormData();
+  ngOnInit() {    
     if(this.parentForm){
       this.parentForm.addControl('additionalDetailsFormGroup', this.FormGrp);
     }
     if (this.isClientViewOnly) {
       this.utils.disableControls(this.FormGrp);
     }
-  }
-
-  initializeFormData() {
-    this.selectedDevice = this.FormGrp.controls.device.value;
-  //  this.makeGetCall("GetClientConfiguration");
-    this.getDevices();
-  }
-
-  async getDevices() {
-    let result = [];
-    let RequestBody: HandleRequest;
-    if (this.selectedDevice == IDTechHandle.name) {
-      RequestBody = {
-        tenderId: PaymentMethods.IDTECH.toString(),
-        inquiryInfo: {
-          cardData: {
-            encryptedData: JSON.stringify({
-              deviceType: 'idtechkb',
-              encryptedData: this.EncryptedCardData
-            })
-          }
-        }
-      }
-    } else {
-      RequestBody = {
-        tenderId: PaymentMethods.CreditCard.toString()
-      };
-    }
-    let userSessionConfiguration: UserSessionConfiguration = await this.userMachineConfigurationService.getUserSessionConfiguration(Number(this.localization.GetPropertyInfo('UserId')));
-    if ((userSessionConfiguration.isIdtechSred || userSessionConfiguration.defaultPaymentDevice == IDTech.id) && !this.availableDevices.includes(IDTechHandle)) {
-      this.availableDevices.push(IDTechHandle);
-    }
-    const Handles: Promise<HandleResponse> = this.payAgentService.GetHandles(RequestBody);
-    Handles.then(response => {
-      if (response && response.status.toLocaleLowerCase() === 'success' && response.paymentHandle.length > 0) {
-        if (this.selectedDevice == IDTechHandle.name && this.EncryptedCardData) {
-          this.CreateToken(response.paymentHandle[0].handle, PaymentMethods.IDTECH);
-        } else {
-          let availableIngenicoDevices = response.paymentHandle;
-          availableIngenicoDevices.map(device => {
-            if (this.availableDevices.filter(x => x.name == device.name).length == 0) {
-              this.availableDevices.unshift(device);
-            }
-          });
-        }
-      }
-      this.SelectDefaultDevice(userSessionConfiguration);
-    });
-  }
-
-  SelectDefaultDevice(userSessionConfiguration) {
-    if (userSessionConfiguration && userSessionConfiguration.defaultPaymentDevice.toLowerCase() == IDTech.id) {
-      this.FormGrp.controls.device.patchValue(IDTechHandle.handle);
-      this.ondeviceChange(IDTechHandle);
-    } else {
-      let defaultDevice = this.availableDevices.find(x => x.name == userSessionConfiguration.defaultDeviceName);
-      if (defaultDevice) {
-        this.FormGrp.controls.device.patchValue(defaultDevice.handle);
-        this.ondeviceChange(defaultDevice);
-      }
-    }
-  }
-
-
-  CloseRoomOpenDialog(data: any): void {
-    this.IDTechCardSwipePopupClosed = true;
-    if (data && this.selectedDevice == IDTechHandle.name) {
-      this.EncryptedCardData = data;
-      this.getDevices();
-    }
-  }
-
-  RoomOpenDialog() {
-    const Popupmessage = this.localization.captions.shop.SwipeCardMessage;
-    const isIDTech: boolean = (this.selectedDevice == IDTechHandle.name);
-    const dataObj = { 'text': Popupmessage, 'buttonname': isIDTech ? this.localization.captions.common.Close : '', 'headertext': '', 'isloaderenable': true, 'isHiddenFieldRequired': isIDTech };
-    this.cardConnectDialog = this.utils.roomOpenDialog(dataObj, this.CloseRoomOpenDialog.bind(this));
-  }
-
-
-
-  async connectDeviceAndGetCardInfo() {
-    let handle: string = "";
-    if (this.selectedDevice != IDTechHandle.name) {
-      this.RoomOpenDialog();
-      handle = this.FormGrp.controls.device.value;
-      this.CreateToken(handle, PaymentMethods.CreditCard);
-    } else {
-      this.CaptureCardWithIDTechDevice();
-    }
-  }
-
-  CreateToken(handle, tenderID) {
-    // Lights up the connected Device and asks for the card swipe
-    const tokentransactionInfo: Promise<TokentransactionInfo> = this.payAgentService.CreateToken(handle, tenderID);
-    tokentransactionInfo.then(response => {
-      if (response.status.toLocaleLowerCase() === 'success') {
-        this.SetSwippedCardInfo(response, tenderID);
-      }
-    }).catch(err => {
-      if (err.error[0] != null && err.error[0].Code != null) {
-        this.payAgentService.PaymentErrorPrompt(err.error[0].Code);
-      }
-      else {
-        return this.utils.ShowError(this.localization.captions.common.Error, this.localization.captions.shop.PMUnexpectedError);
-      }
-      this.cardConnectDialog.close();
-    });
-  }
-
-  async SetSwippedCardInfo(tokentransactionInfo: TokentransactionInfo, tenderID: number) {
-    let swipedcardInfo: ClientCreditCardInfo;
-    let newCardInfo: CardInfo;
-    newCardInfo = {
-      cardNumber: tokentransactionInfo.account.id,
-      cardHolderName: tokentransactionInfo.account.name,
-      entryMode: tokentransactionInfo.cardInfo.entryMode,
-      issuerType: tokentransactionInfo.cardInfo.issuer.toLowerCase(),
-      cardExpiration: tokentransactionInfo.cardInfo.cardExpiration,
-      cardType: tokentransactionInfo.cardInfo.cardType
-    }
-
-    if (!this.payAgentService.ValidateCreditCard(newCardInfo)) {
-      this.cardConnectDialog.close();
-      return;
-    }
-
-    let storeTokenReq: StoreTokenRequest =
-    {
-      cardInfo: newCardInfo,
-      payAgentResponse: {
-        account: tokentransactionInfo.account,
-        payAgentId: tokentransactionInfo.payAgentId,
-        status: tokentransactionInfo.status,
-        transactionDetails: tokentransactionInfo.transactionDetails,
-        transactionKey: tokentransactionInfo.transactionKey,
-      },
-      tenderId: tenderID
-    }
-    let baseResponse: PaymentBaseResponse = await this.getToken(storeTokenReq);
-    if (baseResponse && baseResponse !== null) {
-      swipedcardInfo = {
-        id: 0,
-        tokenTransId: baseResponse.transactionId,
-        isActive: true,
-        clientId: 0,
-        createdTime: this.PropertyInfo.CurrentDTTM
-      };
-      this.cardConnectDialog.close();
-      this.FormGrp.controls.device.markAsDirty();
-      this.FormGrp.controls.device.markAsTouched();
-    } else {
-      // throw error
-      this.cardConnectDialog.close();
-      return;
-    }
-
-    if (this.cardInfo) {
-      this.cardInfo.forEach(x => { x.isActive = false; }); // deactive previously stored card info
-      this.cardInfo = this.cardInfo.filter(c => c.id !== 0); // Remove previously swiped card info
-    }
-    this.cardInfo.push(swipedcardInfo);
-    this.displayCardInfo = newCardInfo;
-    this.displayCardInfo.cardNumber = this.payAgentService.MaskCreditCardNumber(newCardInfo.cardNumber);
-    this.FormGrp.controls.clientCreditCardInfo.setValue(this.cardInfo);
-  }
-
-  formatCreditCardExpiryDate(date: string): string {
-    return this.payAgentService.formatCreditCardExpiryDate(date);
-  }
-
-  async getToken(storeTokenRequest: StoreTokenRequest): Promise<PaymentBaseResponse> {
-    const baseResponse: PaymentBaseResponse = await this.payAgentService.StoreToken(storeTokenRequest);
-    return baseResponse;
-  }
-
-  async getCardInfo(tokenRefId: number): Promise<CardInfo> {
-    return await this.payAgentService.GetCardInfo(tokenRefId);
-  }
+  }  
 
   ngOnDestroy() {
     if (this.formSubscription) {
@@ -294,13 +114,34 @@ export class AdditionalInformationComponent implements OnInit, OnDestroy {
       if (this.cardInfo && this.cardInfo.length > 0) {
         const activeCard = this.cardInfo.filter(x => x.isActive);
         if (activeCard && activeCard.length > 0) {
-          let newCardInfo: CardInfo = await this.getCardInfo(activeCard[0].tokenTransId)
-          if (newCardInfo !== null) {
-            this.displayCardInfo = newCardInfo;
-            this.displayCardInfo.cardNumber = this.payAgentService.MaskCreditCardNumber(newCardInfo.cardNumber);
-          }
+          this.PaymentReferenceID = activeCard[0].tokenTransId;
+          // let newCardInfo: CardInfo = await this.getCardInfo(activeCard[0].tokenTransId)
+          // if (newCardInfo !== null) {
+          //   this.displayCardInfo = newCardInfo;
+          //   this.displayCardInfo.cardNumber = this.payAgentService.MaskCreditCardNumber(newCardInfo.cardNumber);
+          // }
         }
       }
+  }
+
+  SaveReferenceId(event: number) {
+    this.FormGrp.controls.device.markAsDirty();
+    this.FormGrp.controls.device.markAsTouched();
+    this.PaymentReferenceID = event;
+    const swipedcardInfo = {
+      id: 0,
+      tokenTransId: event,
+      isActive: true,
+      clientId: 0,
+      createdTime: this.PropertyInfo.CurrentDTTM
+    };
+    if (this.cardInfo) {
+      this.cardInfo.forEach(x => { x.isActive = false; }); // deactive previously stored card info
+      this.cardInfo = this.cardInfo.filter(c => c.id !== 0); // Remove previously swiped card info
+    }
+    
+    this.cardInfo.push(swipedcardInfo);
+    this.FormGrp.controls.clientCreditCardInfo.setValue(this.cardInfo);
   }
 
   fetchCustomFieldInfo() {
@@ -391,21 +232,6 @@ export class AdditionalInformationComponent implements OnInit, OnDestroy {
     this.FormGrp.controls['customField5'].setValidators(clientConfiguration[0]["CUSTOM_FIELD_5"] && !this.customFieldHidden[4] ? [Validators.required] : []);
     this.FormGrp.controls['customField5'].updateValueAndValidity();
 
-  }
-
-  ondeviceChange(device) {
-    this.selectedDevice = this.FormGrp.controls.device.value;
-  }
-
-  CaptureCardWithIDTechDevice() {
-    this.IDTechCardSwipePopupClosed = false;
-    this.RoomOpenDialog();
-    setTimeout(() => {
-      if (!this.EncryptedCardData && !this.IDTechCardSwipePopupClosed) {
-        this.cardConnectDialog.close();
-        this.utils.ShowError(this.localization.captions.common.Error, this.localization.getError(10725));
-      }
-    }, IDTechCardSwipeTimeout);
-  }
+  }  
 }
 
