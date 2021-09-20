98d1f079198f31cc4a64e6ae9cfefe215507afe3
Rajasekaran Ellappan <Rajasekaran.Ellappan@Agilysys.com>
2021-09-20 20:40:54 +0530

    PMAgent calls errorMsg not showing issue fix

--- src/app/core/services/loader.interceptor.service.ts
+++ src/app/core/services/loader.interceptor.service.ts
@@ -60,7 +60,7 @@ export class LoaderInterceptor implements HttpInterceptor {
                 }),            
             catchError(errs => {
               console.log("catchError -->", errs);
-              return observableThrowError(errs);
+              return throwError(errs);
             }),);       
             
           }
@@ -102,7 +102,3 @@ export class LoaderInterceptor implements HttpInterceptor {
     }
   }
 }
-function observableThrowError(errs: any): any {
-  throw new Error('Function not implemented.');
-}
-
