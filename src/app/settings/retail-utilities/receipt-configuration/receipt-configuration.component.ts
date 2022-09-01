f58649cf9295c2ef945be554cc9bbdfcee28987c
Manickam Saravanan <Manickam.Saravanan@agilysys.com>
2022-09-01 14:36:28 +0530

    toggle changes

--- src/app/settings/retail-utilities/receipt-configuration/receipt-configuration.component.ts
+++ src/app/settings/retail-utilities/receipt-configuration/receipt-configuration.component.ts
@@ -305,7 +305,7 @@ export class ReceiptConfigurationComponent implements OnInit {
     this.isSaveDisabled = true;
   }
   toggleAction(event) {
-    if (event[0] == false) {
+    if (event == false) {
       this.DisplayAuthCode=false;
       this.propertyForm.controls["authcodeName"].setValidators(Validators.required);
       this.propertyForm.controls["authcodeName"].updateValueAndValidity();
