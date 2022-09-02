7bf27ab084922830357f79ce1b7248dc4083c690
raveendranh <Harishchandar.Raveendran@agilysys.com>
2022-09-02 14:16:07 +0530

    Safe check for PostType Mapping

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -426,11 +426,28 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   async setEatecConfig(){
     this.propertyFeatureService.getPropertyFeatures().then( async (feature) => {
+      const propIds = [];
+
       const eatecFeature = feature.find(x => x.featureName === FeatureName.EnhancedInventory);
-      if (eatecFeature != null && eatecFeature.isActive) {
+      const pmsRevenuePosting = feature && feature.find(x => x.featureName === FeatureName.PMS_RevenuePosting && x.isActive);
+
+      if (eatecFeature) {
         sessionStorage.setItem('isEatecEnabled', 'true');
-        this.propertyFeatureService.getFeatureConfiguration(eatecFeature.id,eatecFeature.moduleId).then((featureconfigurations) => {
-          if (featureconfigurations != null && featureconfigurations.length > 0) {
+        propIds.push(eatecFeature.id);
+        await this.setEatecToken();
+      } else {
+        sessionStorage.setItem('isEatecEnabled', 'false');
+        this.retailpropertyInfo.SetEatecRI('');
+      }
+
+      if (pmsRevenuePosting) {
+        propIds.push(pmsRevenuePosting.id);
+      }
+
+      if (propIds.length > 0) {
+        sessionStorage.setItem('isEatecEnabled', 'true');
+        this.propertyFeatureService.GetFeatureConfigurationsById(propIds).then((featureconfigurations) => {
+          if (eatecFeature && featureconfigurations != null && featureconfigurations.length > 0) {
             const eatecUser = featureconfigurations.find(f => f.configurationKey === ConfigKeys.Eatec.EatecTenantUser);
             const uri = featureconfigurations.find(f => f.configurationKey === ConfigKeys.Eatec.EatecURI);
             if (eatecUser && eatecUser.configurationValue && uri && uri.configurationValue) {
@@ -441,12 +458,15 @@ export class LoginComponent implements OnInit, OnDestroy {
           }else {
             this.retailpropertyInfo.SetEatecRI('');
           }
+         
+          if (pmsRevenuePosting && featureconfigurations != null && featureconfigurations.length > 0) {
+            const pmsSystem = featureconfigurations.find(f => f.configurationKey === ConfigKeys.PMSRevenuePosting.PMSSystem)?.configurationValue;
+            if (pmsSystem && pmsSystem != null) {    
+              sessionStorage.setItem('pmsSystem', pmsSystem);
+          }
+        }
         });
-        await this.setEatecToken();
-      } else {
-        sessionStorage.setItem('isEatecEnabled', 'false');
-        this.retailpropertyInfo.SetEatecRI('');
-      }
+      }       
     });
   }
 
