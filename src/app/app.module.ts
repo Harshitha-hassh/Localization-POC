73e36b511dd6f5715f6ee457f5a071b5c2062bf5
Mohamed Haris Mohamed Illiyas <Mohamed.Haris@agilysys.com>
2022-09-19 11:55:41 +0000

    Merged PR 66929: Retail Standalone : #81334 Auth Issue Fix
    - Retail Standalone UI : #81334 Auth Issue Fix
    
    Related work items: #81334

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,7 +21,6 @@ import { ServiceLocator } from './common/service.locator';
 import { AppModuleService } from './core/services/app.service';
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { GlobalErrorHandler } from './shared/service/global-error-handler.service';
-import { ADB2CAuthConfiguration } from './login/auth.config';
 let AppServiceFactory = (utilities: Utilities, localization: RetailStandAloneLocalization) => {
   return new RetailAppService(utilities, localization);
 };
@@ -60,8 +59,7 @@ declare module "@angular/core" {
     RetailPropertyInformation,
     { provide: Localization, useExisting: RetailStandAloneLocalization },
     { provide: CommonPropertyInformation, useExisting: RetailPropertyInformation },
-    AppModuleService,
-    ADB2CAuthConfiguration
+    AppModuleService
   ],
   bootstrap: [AppComponent]
 })
