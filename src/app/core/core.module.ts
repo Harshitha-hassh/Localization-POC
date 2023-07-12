7d7b40ebf22f1a58053aea0a42010e698876be1c
Swetha M <swetha.m@agilysys.com>
2023-07-12 19:06:38 +0530

    Bug Fix 97376

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouteLoaderService } from './services/route-loader.service';
 import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from '../shared/shared.module';
+import { CanDeactivateGuardService } from './services/can-component-deactivate.service';
 
 
 
@@ -19,6 +20,7 @@ import { SharedModule } from '../shared/shared.module';
     ],
     providers: [
         RouteLoaderService,
+        CanDeactivateGuardService,
     ],
     exports: []
 })
