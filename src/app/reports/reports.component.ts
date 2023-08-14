e870dc301e3ec45da376b8f131c79cd5dc3ab050
Hema Sree Sivanesan <HemaSree.Sivanesan@agilysys.com>
2023-08-14 14:24:53 +0000

    Merged PR 93302: Inventory control menu hide under reports
    Inventory control menu hide under reports
    
    Related work items: #99587

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { menuTypes } from '../shared/enums/menu.constant';
 import { RouteLoaderService } from '../core/services/route-loader.service';
-
+import { RetailPropertyInformation } from '../retail/common/services/retail-property-information.service';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
@@ -9,16 +9,21 @@ import { RouteLoaderService } from '../core/services/route-loader.service';
   encapsulation: ViewEncapsulation.None
 })
 export class ReportsComponent implements OnInit {
-
+  menu :any;
   menuList: any;
   menuType = menuTypes;
-  constructor(private routeDataService: RouteLoaderService) {
+  constructor(private routeDataService: RouteLoaderService, private propertyInfo : RetailPropertyInformation) {
    }
 
   async ngOnInit() {
     const value = this.routeDataService.GetChildMenu('/reports');
+    this.menu = value.linkedElement;
+    if(!this.propertyInfo.IsEatecEnabled) {
+      let configsToRemove = ['/reports/inventorycontrol'];
+      this.menu = this.menu.filter(r => !configsToRemove.includes(r.routePath));
+    } 
     this.menuList = {
-      menu: value.linkedElement,
+      menu: this.menu,
       menuType :  value.linkedElement[0].menuAlignment
     };
   }
