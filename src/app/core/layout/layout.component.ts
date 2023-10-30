42c4d9d748e0515b591662b8933eef44c1d23f00
Rajasekaran Ellappan <Rajasekaran.Ellappan@agilysys.com>
2023-10-30 06:51:00 +0000

    Merged PR 100105: Changed Constant for build issue fix
    Changed Constant for build issue fix
    
    Related work items: #79864

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -117,7 +117,7 @@ export class LayoutComponent implements OnInit, OnDestroy {
     let previousDate = moment(newSystemDate).subtract(1,"days");
       let localizedPreviousDate = this.localization.localizeDisplayDate(previousDate.toDate());
     if (newSystemDate != undefined && newSystemDate != null) {
-      if(productId == Product.SPA || productId == Product.Golf || productId == Product.RETAIL )
+      if(productId == Product.SPA || productId == Product.GOLF || productId == Product.RETAIL )
       {
         this.utils.showToastMessage(this.localization.captions.NightAuditMessage+ ' for ' + localizedPreviousDate, SnackBarType.Success, 15000);
       }
