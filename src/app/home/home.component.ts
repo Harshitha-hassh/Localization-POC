c5fedcc8f5c9982949943b6bd5ae56d0c8553842
Alaguraj, Anu <Anu.Alaguraj@agilysys.com>
2025-08-12 12:15:44 +0000

    Merged PR 172311: changes in date roll notification
    Related work items: #180045

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonVariablesService } from '../retail/shared/service/common-variables.service';
 import { RetailUtilities } from '../retail/shared/utilities/retail-utilities';
-import { RetailPropertyInformation } from '../core/services/retail-property-information.service';
 import { AlertType } from '../retail/shared/shared.modal';
 import { RetailLocalization } from '../retail/common/localization/retail-localization';
 import * as GlobalConst from '../retail/shared/globalsContant';
@@ -15,7 +14,7 @@ import * as GlobalConst from '../retail/shared/globalsContant';
 export class HomeComponent implements OnInit {
 
   constructor(private localization: RetailLocalization, public _CommonVariablesService: CommonVariablesService, 
-    public _RetailUtilities: RetailUtilities, public _RetailPropertyInformation: RetailPropertyInformation
+    public _RetailUtilities: RetailUtilities
   ) { }
 
   ngOnInit() {
@@ -31,11 +30,9 @@ export class HomeComponent implements OnInit {
     }
 
     const property = await this._CommonVariablesService.GetPropertySettings();
-    const isSystemDateEqual = this._RetailUtilities.ValidateDatesAreEqual(this._RetailUtilities.getDate(property.propertyDate), this._RetailPropertyInformation.CurrentDate);
+    const isSystemDateEqual = this._RetailUtilities.ValidateDatesAreEqual(this._RetailUtilities.getDate(property.propertyDate), this.localization.getCurrentDate());
     if (!isSystemDateEqual) {
-      this._RetailUtilities.showAlert(this.localization.getError(-4702), AlertType.Warning, GlobalConst.ButtonType.Ok, (res) => {
-        console.log('Date mismatch notification shown');
-      });
+      this._RetailUtilities.showAlert(this.localization.getError(-4702), AlertType.Warning, GlobalConst.ButtonType.Ok);
     }
   }
 }
