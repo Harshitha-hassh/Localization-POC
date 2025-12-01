26c230905dcc444f7717eb11127fee1619f01c42
Rajkumar, Komathan <Komathan.Rajkumar@agilysys.com>
2025-12-01 15:43:31 +0000

    Merged PR 186207: Allow token sharing key added
    Allow token sharing key added
    
    Related work items: #210591

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -692,6 +692,16 @@ export class LoginComponent implements OnInit, OnDestroy {
     this.localize.SetLocaleBasedProperties();
     this.commonLocalize.SetLocaleBasedProperties();
     this.UpdateUserRole(Selectedproperty.id);
+    await this.SetAllowTokenSharing();
+  }
+
+ async SetAllowTokenSharing() {
+    let allowTokenSharing = false;
+    const configuration = await this.configuration.GetTenantConfiguration();
+    if (configuration?.configValue) {
+      allowTokenSharing = configuration.configValue?.AllowTokenSharing ? configuration.configValue.AllowTokenSharing.toLowerCase() == 'true' : false;
+    }
+    sessionStorage.setItem('AllowTokenSharing', allowTokenSharing.toString());
   }
 
   async setEatecConfig() {
